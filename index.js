5a9aeda3ac870cdc94aa511bf2310d6ecaf4d3b2
RashidulIslam0 <aboutrashidul@gmail.com>
2024-07-13 10:44:00 +0600

    server error solve

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-// dependencies
-
+//
 const http = require('http');
-
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-    port: 3100,
+    PORT: 3100,
 };
 
-
 // create server
+app.createServer = () => {
+    const server = http.createServer(app.handleReqRes);
+    server.listen(app.config.PORT, () => {
+        console.log(`http://localhost:${app.config.PORT}`);
+    });
+};
 
+app.handleReqRes = (req, res) => {
+    res.end("Hello World");
+};
 
-app.createServer=()=>{
-    const server =http.createServer()
-    server.listen(app.config.port,()=>{
-        console.log(`listening to port ${app.config.port}`)
-    })
-}
-
-app.handleReqRes=(req,res)=> {
-    res.end("Hello World")
-}
-
-app.createServer()
+app.createServer();
